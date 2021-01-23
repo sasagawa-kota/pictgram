7545d7e49b10d4c12dc56c28f281d086b935c89b
sasagawa-kouta <sasagawa.kota0924@gmail.com>
2021-01-23 14:26:13 +0000

    デプロイ1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
     
     private
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :topic_id, :user_id)
     end
 end
   
